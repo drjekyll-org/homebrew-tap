57b740409ef8f60b9db985299e68d69cdbf2ca2d
Daniel Heid <mail@daniel-heid.de>
2026-06-04 12:24:28 +0000

    Update adocfmt to 0.1.1

--- Formula/adocfmt.rb
+++ Formula/adocfmt.rb
@@ -1,9 +1,9 @@
 class Adocfmt < Formula
   desc "An opinionated AsciiDoc formatter"
   homepage "https://github.com/dheid/adocfmt"
-  url "https://github.com/dheid/adocfmt/releases/download/v0.1.0/adocfmt.jar"
-  version "0.1.0"
-  sha256 "9309746fb69599ed8d1eda48b561038a979634656244053db13831779f83f80d"
+  url "https://github.com/dheid/adocfmt/releases/download/v0.1.1/adocfmt.jar"
+  version "0.1.1"
+  sha256 "9cbca81a7bdebe39abcc63938397cb8db0b5a36a83d7dc2078044230c63addc9"
   license "Apache-2.0"
 
   depends_on "openjdk@17"
